260c61da7ca9fe97b8ab6cf44f99c190d8866879
System Administrator <root@iMac-de-Rodrigo.local>
2019-03-23 22:25:25 -0300

    entrega 1

--- js/extras.js
+++ js/extras.js
@@ -1,9 +1,9 @@
 var objetos = new Array();
 var azar = 0;
-objetos[0] = "img/header.jpg";
-objetos[1] = "img/header1.jpg";
-// objetos[2] = "img/header2.jpg";
-// objetos[3] = "img/header3.jpg";
+objetos[0] = "img/headerA.jpg";
+objetos[1] = "img/header1A.jpg";
+objetos[2] = "img/header2A.jpg";
+//objetos[3] = "img/header.png";
 // objetos[4] = "img/header4.jpg";
 // objetos[5] = "img/header5.jpg";
 // objetos[6] = "img/header6.jpg";
@@ -13,6 +13,5 @@ function aleatorio() {
 	var azar = Math.floor(Math.random() * objetos.length);
     background = document.querySelector(".masthead");
     background.style.backgroundImage = "url(" + objetos[azar] + ")";
-	  
 }
 
